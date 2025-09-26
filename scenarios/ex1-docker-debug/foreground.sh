3f36a72d3c1e41998882a5eec6fdf53f1d119ab1
pluc87 <luc.payen@gmail.com>
2025-09-26 14:55:59 +0200

    feat: Make setup scripts executable

--- scenarios/ex1-docker-debug/foreground.sh
+++ scenarios/ex1-docker-debug/foreground.sh
@@ -1,12 +1,14 @@
 #!/usr/bin/env bash
+set -euo pipefail
 
-# Exécute le script d'initialisation
-env-init.sh
-
-# Créer le répertoire de travail pour l'utilisateur
-mkdir -p /root/ex1/app
+# SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+# bash "${SCRIPT_DIR}/env-init.sh"
+# L'appel au script d'initialisation est mieux géré par Killercoda directement.
+# On va le lancer via la section "intro" du index.json.
 
 # Message pour l'utilisateur
 printf "\n✅ Environnement prêt. Les fichiers de l'exercice se trouvent dans /root/ex1\n"
-printf "Voici l'arborescence :\n"
+printf "Voici l'arborescence des fichiers :\n"
+
+# La commande 'tree' est parfaite pour vérifier que les assets ont bien été copiés.
 tree /root/ex1
